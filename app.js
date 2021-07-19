83550f1b34c4c7ec641b398ff614f9597a34e99f
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-07-19 23:06:24 +0530

    input output processing done

--- app.js
+++ app.js
@@ -4,11 +4,13 @@
 // let userName = prompt(`hi user emter your name`);
 // alert(`hi ${userName}`);
 
-let myButton = document.querySelector("#translate");
 let textInput = document.querySelector("#textInput");
+let outputDiv = document.querySelector(`.output`)
+let myButton = document.querySelector("#translate");
 
-myButton.addEventListener(`click`, function(e){
-    // console.log(e);
-    // console.log(`${e} ,i got clicked`);
-    console.log(`${textInput.value}`);
+// outputDiv.innerText = `i'm captain america`
+
+myButton.addEventListener(`click`, function (e) {
+    // console.log(`${textInput.value}`);
+    outputDiv.innerText = `asjsjjsjsjsjsj ${textInput.value}` ; 
 });
