0f6c944cb5b5ed39d276bc0ed256d00603e63d65
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-02 23:00:19 +0530

    add styling

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ myButton.addEventListener(`click`, function (e) {
     // console.log(`${textInput.value}`);
     // outputDiv.innerText = `asjsjjsjsjsjsj ${textInput.value}` ; 
     // const url = `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput.value}`;
-    const url = `https://api.funtranslations.com/translate/minion.json`;
+    const url = `https://api.funtranslations.com/translate/minion.json?text=${textInput}`;
 
     fetch(url)
         .then(response => response.json())
-        .then(json => console.log(json));
-        // .then(json => outputDiv.innerText = json.contents.translation);
+        // .then(json => console.log(json));
+        .then(json => outputDiv.innerText = json.contents.translation);
 });
